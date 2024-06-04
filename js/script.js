53e42ed354cababdf75a01b179c29bf0c743b560
FraCuboni <fracuboni@gmail.com>
2024-06-04 15:51:13 +0200

    added discounts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 // stabilisco il prezzo al kilometro
 const prezzoKm = 0.21;
 
+// sconto under18
+const sconto18 = 0.20;
+
+// sconto over65
+const sconto65 = 0.40
+
 // chiedo il chilometraggio
 let chilometraggio = parseInt(prompt("quanti Km devi viaggiare?"));
 
@@ -12,15 +18,15 @@ let prezzoAlKm = prezzoKm * chilometraggio;
 console.log("Il prezzo per il chilometraggio selezionato è:", prezzoAlKm)
 
 // chiedo l'età
-let eta = parseInt(prompt("quanti anni hai?"));
+let etaUtente = parseInt(prompt("quanti anni hai?"));
 
-console.log("l'età dell utente è:",eta);
+console.log("l'età dell utente è:",etaUtente);
 
 // calcolo se applicare sconti per l'età
-if(eta<18){
+if(etaUtente<18){
 
     console.log("Hai diritto a uno sconto del 20%!")
-}else if(eta>65){
+}else if(etaUtente>65){
 
     console.log("Hai diritto a uno sconto del 40%!")
 }else{
