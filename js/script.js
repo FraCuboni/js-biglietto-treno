90de61e0c95432dc300e0dca11a1e6e8c9be8cc1
FraCuboni <fracuboni@gmail.com>
2024-06-04 15:41:22 +0200

    everything connected math still missing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 // stabilisco il prezzo al kilometro
+const prezzoKm = 0.21;
 
 // stabilisco sconto per età
 
 // chiedo il chilometraggio
+let chilometraggio = prompt("quanti Km devi viaggiare?");
+
+console.log("il chilometraggio scelto dall'utente è:",chilometraggio);
 
 // calcolo il prezzo in base alla distanza
 
 // chiedo l'età
+let eta = prompt("quanti anni hai?");
+
+console.log("l'età dell utente è:",eta);
 
 // calcolo se applicare sconti per l'età
+if(eta<18){
 
+    console.log("Hai diritto a uno sconto del 20%!")
+}else if(eta>65){
+
+    console.log("Hai diritto a uno sconto del 40%!")
+}else{
+
+    console.log("Non hai diritto a uno sconto!")
+}
 // approssimo il numero a due decimali
 
 // do il prezzo finale
