aa4141f8282a01292e099d18058a30b36b1bc26e
FraCuboni <fracuboni@gmail.com>
2024-06-04 15:58:09 +0200

    added math to discounts

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // stabilisco il prezzo al kilometro
-const prezzoKm = 0.21;
+const prezzoAlKm = 0.21;
 
 // sconto under18
-const sconto18 = 0.20;
+const sconto18 = 0.80;
 
 // sconto over65
-const sconto65 = 0.40
+const sconto65 = 0.60
 
 // chiedo il chilometraggio
 let chilometraggio = parseInt(prompt("quanti Km devi viaggiare?"));
@@ -13,9 +13,9 @@ let chilometraggio = parseInt(prompt("quanti Km devi viaggiare?"));
 console.log("il chilometraggio scelto dall'utente è:",chilometraggio);
 
 // calcolo il prezzo in base alla distanza
-let prezzoAlKm = prezzoKm * chilometraggio;
+let prezzoKm = prezzoAlKm * chilometraggio;
 
-console.log("Il prezzo per il chilometraggio selezionato è:", prezzoAlKm)
+console.log("Il prezzo per il chilometraggio selezionato è:", prezzoKm)
 
 // chiedo l'età
 let etaUtente = parseInt(prompt("quanti anni hai?"));
@@ -25,10 +25,14 @@ console.log("l'età dell utente è:",etaUtente);
 // calcolo se applicare sconti per l'età
 if(etaUtente<18){
 
-    console.log("Hai diritto a uno sconto del 20%!")
+    let prezzo = prezzoKm * sconto18;
+    console.log("Hai diritto a uno sconto del 20%! Nuovo prezzo:", prezzo)
+
 }else if(etaUtente>65){
 
-    console.log("Hai diritto a uno sconto del 40%!")
+    let prezzo = prezzoKm * sconto65;
+    console.log("Hai diritto a uno sconto del 40%! Nuovo prezzo:", prezzo)
+
 }else{
 
     console.log("Non hai diritto a uno sconto!")
