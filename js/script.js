842bb33e98ea8d570d59dcd819f793ce1ecda2b6
FraCuboni <fracuboni@gmail.com>
2024-06-04 15:25:44 +0200

    schema base

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// stabilisco il prezzo al kilometro
+
+// stabilisco sconto per età
+
+// chiedo il chilometraggio
+
+// calcolo il prezzo in base alla distanza
+
+// chiedo l'età
+
+// calcolo se applicare sconti per l'età
+
+// approssimo il numero a due decimali
+
+// do il prezzo finale
